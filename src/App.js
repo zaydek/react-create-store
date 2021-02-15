2b04f3022553a5d27b15580bd872c4788feb486e
Zaydek Michels-Gualtieri <zaydekdotcom+codex-zaydek@gmail.com>
2021-02-16 01:01:33 +0900

    MVP funcs implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import createStore from "./createStore"
+import { createStore, useStore, useStoreValue } from "./createStore"
 
-const useStore = createStore("Hello, world!")
+// Creates a new four-character hash ID.
+function newID() {
+	return Math.random().toString(36).slice(2, 6)
+}
+
+const initialState = {
+	done: false,
+	text: "",
+	todos: [],
+}
+
+const store = createStore(initialState)
+
+const reducer = state => ({
+	addTodo() {
+		if (state.text === "") {
+			// No-op
+			return
+		}
+		const todo = { id: newID(), done: state.done, text: state.text }
+		const done = false // Reset
+		const text = "" // Reset
+		return { ...state, done, text, todos: [todo, ...state.todos] }
+	},
+	setDone(done) {
+		return { ...state, done }
+	},
+	setText(text) {
+		return { ...state, text }
+	},
+	setDoneByID(id, done) {
+		const todos = state.todos.map(each => {
+			if (each.id === id) {
+				return { ...each, done }
+			}
+			return each
+		})
+		return { ...state, todos }
+	},
+	setTextByID(id, text) {
+		const todos = state.todos.map(each => {
+			if (each.id === id) {
+				return { ...each, text }
+			}
+			return each
+		})
+		return { ...state, todos }
+	},
+	removeByID(id) {
+		const todos = state.todos.filter(each => each.id !== id)
+		return { ...state, todos }
+	},
+})
 
 function App() {
-	const [state, setState] = useStore()
+	const [state, funcs] = useStore(store, reducer)
+
+	function handleSubmit(e) {
+		e.preventDefault()
+		funcs.addTodo()
+	}
+
 	return (
 		<div>
-			<h1>{state}</h1>
-			<input type="text" value={state} onChange={e => setState(e.target.value)} />
+			{/* eslint-disable-next-line no-sequences */}
+			<form onSubmit={handleSubmit}>
+				<input type="checkbox" checked={state.done} onChange={e => funcs.setDone(e.target.checked)} />
+				<input type="text" value={state.text} onChange={e => funcs.setText(e.target.value)} />
+				<button type="submit">+</button>
+			</form>
+			{state.todos.map(each => (
+				<div key={each.id}>
+					<input type="checkbox" checked={each.done} onChange={e => funcs.setDoneByID(each.id, e.target.checked)} />
+					<input type="text" value={each.text} onChange={e => funcs.setTextByID(each.id, e.target.value)} />
+					<button onClick={() => funcs.removeByID(each.id)}>-</button>
+				</div>
+			))}
 		</div>
 	)
 }
 
 function AppInfo() {
-	const [state] = useStore()
-	return (
-		<div>
-			<pre>{JSON.stringify({ state }, null, 2)}</pre>
-		</div>
-	)
+	const state = useStoreValue(store)
+	return <pre>{JSON.stringify({ state }, null, 2)}</pre>
 }
 
 export default function AppRoot() {
--- src/createStore.js
+++ src/createStore.js
@@ -3,36 +3,78 @@ import { useCallback, useEffect, useState } from "react"
 // This implementation is inspired by react-create-shared-state.
 // https://github.com/mucsi96/react-create-shared-state
 
-// Creates a store that returns [state, setState]. A store manages subscriptions
-// and broadcasts state changes to subscriptions.
-export default function createStore(initialState) {
+const internalStoreReference = {}
+
+const badStoreError = new Error(
+	"useStore: Bad store. " +
+		"Use createStore(initialStateOrInitializer) to create a new store and then use const [state, setState] = useStore(store).",
+)
+
+// Tests a store for internalStoreReference.
+function testStore(store) {
+	return store && store.__type__ && store.__type__ === internalStoreReference
+}
+
+// Creates a new store.
+export function createStore(initialStateOrInitializer) {
 	const subscriptions = new Set()
 
+	const initialState =
+		typeof initialStateOrInitializer === "function" ? initialStateOrInitializer() : initialStateOrInitializer
+
 	// Caches the current state for when a component mounts; see
-	// useState(cachedState).
+	// useState(store.cachedState).
 	let cachedState = initialState
 
-	function useStore() {
-		const [state, setState] = useState(cachedState)
+	return { __type__: internalStoreReference, subscriptions, initialState, cachedState }
+}
 
-		// Effect that manages subscriptions when a component mounts / unmounts.
-		useEffect(() => {
-			subscriptions.add(setState)
-			return () => {
-				subscriptions.delete(setState)
-			}
-		}, [])
-
-		// Synthetic setState that broadcasts state changes to subscriptions.
-		const syntheticSetState = useCallback(action => {
-			const nextState = typeof action === "function" ? action(cachedState) : action
-			cachedState = nextState
-			setState(nextState)
-			subscriptions.forEach(each => each(nextState))
-		}, [])
-
-		return [state, syntheticSetState]
+// Uses a store; returns a state and setState accessor.
+export function useStore(store, reducer = null) {
+	if (!testStore(store)) {
+		throw badStoreError
 	}
 
-	return useStore
+	// TODO: Add testReducer.
+
+	const [state, setState] = useState(store.cachedState)
+
+	// Effect that manages subscriptions when a component mounts / unmounts.
+	useEffect(() => {
+		store.subscriptions.add(setState)
+		return () => {
+			store.subscriptions.delete(setState)
+		}
+	}, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+	// Synthetic setState that broadcasts state changes to subscriptions.
+	const syntheticSetState = useCallback(action => {
+		const nextState = typeof action === "function" ? action(store.cachedState) : action
+		store.cachedState = nextState
+		setState(nextState)
+		store.subscriptions.forEach(each => each(nextState))
+	}, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+	if (reducer !== null) {
+		const reduce = reducer(state)
+		const keys = Object.keys(reduce)
+		const syntheticDispatch = keys.reduce((dispatch, action) => {
+			dispatch[action] = function (/* Uses (...arguments) */) {
+				syntheticSetState(reduce[action](...arguments))
+			}
+			return dispatch
+		}, {})
+		return [state, syntheticDispatch]
+	}
+	return [state, syntheticSetState]
+}
+
+// Uses a store; returns a state accessor.
+export function useStoreValue(store) {
+	return useStore(store)[0]
+}
+
+// Uses a store; returns a setState accessor.
+export function useStoreSetState(store, reducer = null) {
+	return useStore(store, reducer)[1]
 }
